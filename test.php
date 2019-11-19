53e38ab6474df4cbcaa6db3eadad9070e9368e51
fish0406 <57655629+fish0406@users.noreply.github.com>
2019-11-19 16:17:59 +0800

    Update test.php

--- test.php
+++ test.php
@@ -5,16 +5,10 @@
 <body>
 <?php
 
-
-
 echo "hello world!<br>";
-<<<<<<< HEAD
-
 echo "我是思婷";
-=======
 echo "我是呂明 ㄎㄎ!<br>";
 
->>>>>>> 44eafab7c3fd0394201d06d06cedc55b1beeeb70
 ?>
 </body>
 </html>
